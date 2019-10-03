962180095ca27314f80a3d02d42e64f4f42c3961
Nelvake <NickSagalbaev@gmail.com>
2019-10-03 14:33:48 +0600

    Добавлен класс FileInfo приходящих файлов.

---
+++ ImageCutAndSearch/ImageCutAndSearch/FileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ImageCutAndSearch
+{
+    public class FileInfo
+    {
+        public byte[] PhotoBytes { get; set; }
+        public string FileName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Coordinates { get; set; }
+        public string Tag { get; set; }
+
+        public FileInfo(string path)
+        {
+            PhotoBytes = File.ReadAllBytes(path);
+        }
+    }
+}
