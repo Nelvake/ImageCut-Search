ede0f844ccfb944a4e45be167e598a737302098a
Nelvake <NickSagalbaev@gmail.com>
2019-10-03 14:35:10 +0600

    Наработки.

--- ImageCutAndSearch/ImageCutAndSearch/Program.cs
+++ ImageCutAndSearch/ImageCutAndSearch/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
+using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace ImageCutAndSearch
 {
@@ -6,7 +12,51 @@ namespace ImageCutAndSearch
     {
         static void Main(string[] args)
         {
-            
+            List<FileInfo> images = new List<FileInfo>(5);
+
+
+            var rootDirectory = @"C:\New\";
+            List<string> filesPath = Directory.GetFiles(rootDirectory).ToList();
+            //ManualInput(images, filesPath);
+            AutoInit(images, filesPath);
+
+
+
+        }
+
+        private static void ManualInput(List<FileInfo> images, List<string> filesPath)
+        {
+            foreach (var file in filesPath)
+            {
+                images.Add(new FileInfo(file)
+                {
+                    Country = Console.ReadLine(),
+                    City = Console.ReadLine(),
+                    Tag = Console.ReadLine(),
+                    Coordinates = "327285"
+                });
+                FileService.InitFileName(images.Last());
+            }
+        }
+
+        private static void AutoInit(List<FileInfo> images, List<string> filesPath)
+        {
+            images.Add(new FileInfo(filesPath[0])
+            {
+                Country = "Казахстан",
+                City = "Астана",
+                Tag = "Парк",
+                Coordinates = "12843930"
+            });
+            FileService.InitFileName(images.Last());
+            images.Add(new FileInfo(filesPath[1])
+            {
+                Country = "Казахстан",
+                City = "Костанай",
+                Tag = "Парк",
+                Coordinates = "384020843"
+            });
+            FileService.InitFileName(images.Last());
         }
     }
 }
