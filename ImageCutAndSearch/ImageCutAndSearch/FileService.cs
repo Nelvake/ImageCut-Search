c3d44dfbbddd852832e2def6b4ef0c9e3871d6db
Nelvake <NickSagalbaev@gmail.com>
2019-10-03 14:34:52 +0600

    Добавлен класс FileService для работы с классом FileInfo

---
+++ ImageCutAndSearch/ImageCutAndSearch/FileService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageCutAndSearch
+{
+    public class FileService
+    {
+        /// <summary>
+        /// Инициализация имени файла
+        /// </summary>
+        /// <param name="fileInfo"></param>
+        /// <returns></returns>
+        public static FileInfo InitFileName(FileInfo fileInfo)
+        {
+            fileInfo.FileName = $"[{fileInfo.Country}][{fileInfo.City}][{fileInfo.Tag}][{fileInfo.Coordinates}].";
+            return fileInfo;
+        }
+    }
+}
